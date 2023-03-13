84818761f3c3b5fd4fed5d4bbb188f2548294e86
Cristián Pérez <cristian@wenoa.studio>
2023-03-13 18:51:00 -0300

    feat: mongo6

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]
+then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+VERSION="$1"
+
+docker buildx build --platform linux/amd64,linux/arm64 --build-arg VERSION="$VERSION" --tag "cperezabo/mongors:$VERSION" --push .
+
+# In case of:
+# ERROR: multiple platforms feature is currently not supported for docker driver. Please switch to a different driver (eg. "docker buildx create --use")
+# Be sure of using a builder instance that supports the specified platforms.
+# In my case I just created one using the following command:
+# `docker buildx create --name apple-silicon-intel --platform linux/amd64,linux/arm64``
+# And made Docker use it by running:
+# `docker builx use apple-silicon-intel`
+# Then re-run this script.
--- rs-initiate.sh
+++ rs-initiate.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-until mongo --eval "rs.initiate()";
+until mongosh --eval "rs.initiate()";
 do
     sleep 0.01;
 done
 
-mongo --eval "rs.reconfig({
+mongosh --eval "rs.reconfig({
     _id : \"rs0\",
     members: [
         { _id: 0, host: \"${PRIMARY_HOST:=localhost}\" }
